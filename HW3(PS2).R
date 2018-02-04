a20bf8c687edf85f29b70d56fa452c19548a2ba8
Gangyi Sun <gangyi.sun@wustl.edu>
2018-02-04 12:58:07 -0600

    Added functions to calculate m and d statistics.

--- HW3(PS2).R
+++ HW3(PS2).R
@@ -31,6 +31,34 @@ DigitDist<-function(x){
   return(IntCount)          # returns IntCount as the output of the function
 }
 
+# The StatM function takes as input x, which is a matrix of first significant digit integer 
+# frequencies (output from DigistDist).
+# The function returns the Leemis' m statistic.  
+StatM<-function(x){
+  Mi<-NA
+  for (i in 1:9){           # evaluates Xi-log10(1+1/i) for each integer i, stored in vector Mi
+    Xi<-IntCount[i]/sum(IntCount)     # Xi is the proportional frequency of integer i observed in x 
+    Mi[i]<-Xi-log10(1+1/i)
+  }
+  m<-max(Mi)                # the Leemis' m statistic is the maximum value contained in vector Mi
+  return(m)                 # returns m as the output of the function 
+}
+
+# The StatD function takes as input x, which is a matrix of first significant digit integer 
+# frequencies (output from DigistDist).
+# The function returns the Cho-Gains' d statistic.  
+StatD<-function(x){
+  Di<-NA
+  for (i in 1:9){           # evaluates (Xi-log10(1+1/i))^2 for each integer i, stored in vector Mi
+    Xi<-IntCount[i]/sum(IntCount)     # Xi is the proportional frequency of integer i observed in x 
+    Di[i]<-(Xi-log10(1+1/i))^2
+  }
+  d<-sqrt(sum(Di))          # the Cho-Gains' d statistic is the sum of all values contained in vector Di
+  return(d)                 # returns d as the output of the function 
+}
 
 x<-c(1234,125356,1,234,222,3,34567,4567,498,5555,5987,61234,671234,71,74,88,80,95,99999,9)
-DigitDist(x)
+xDist<-DigitDist(x)
+StatM(xDist)
+StatD(xDist)
+
